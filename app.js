20cac44a5f4752aa003cb5aeb6689bde7a990f17
Julio <juliolk.4321@gmail.com>
2022-03-25 14:26:20 -0300

    Adicionando express e nodemon

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 const { principal } = require('./src/index')
 
 principal()
+
+const express = require('express')
+const app = express()
+const port = 3500
+
+app.get('/gaiaorganicos', (req, res) => {
+    res.send('lojinha de rua hey')
+  })
+
+app.listen(port, function (){
+    console.log('Servidor funcionando na porta' + port)
+})
