c5956f3d47e65c6095e9a2dafa13118d954bc1c6
juvigh <juliolk.4321@gmail.com>
2022-03-30 21:28:21 -0300

    atualizando (entrada html)

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ const { principal } = require('./src/index')
 principal()
 
 const express = require('express')
+const path = require("path")
+const { dirname } = require('path/posix')
 const app = express()
 const port = 3500
 
+app.use('/', express.static(path.join(__dirname, 'public/')))
+
 app.get('/gaiaorganicos', (req, res) => {
     res.send('lojinha de rua hey')
-  })
+  });
 
 app.listen(port, function (){
     console.log('Servidor funcionando na porta' + port)
-})
+});
